a5c6aecc6227420b804438d2f2ecc5b9bea7eb1e
Ephrem Sisay <ephisismu@gmail.com>
2021-11-24 11:40:10 -0800

    adding files

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    [print("{}: {}".format(x, a_dictionary[x])) \
-for x in sorted(a_dictionary)]
+    for llave in sorted(a_dictionary):
+        print("{}: {}".format(llave, a_dictionary[llave]))
