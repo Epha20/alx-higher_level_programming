ebad446953c4ff9fad58f15be782502ac49c6ba6
Ephrem Sisay <ephisismu@gmail.com>
2021-12-22 07:56:00 -0800

    adding files

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+"""
+    2-is_same_class: is_same_class()
+"""
 
 
 def is_same_class(obj, a_class):
-    """'''function: is_same_class
-    obj: an object
-    a_class: a class
-    Returns: Bool
     """
-    return type(obj) == a_class
+        is_same_class returns True if object is an instance of specified class.
+        Args:
+            obj (object): object being checked.
+            a_class (class): class.
+        Return: True or False.
+    """
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
